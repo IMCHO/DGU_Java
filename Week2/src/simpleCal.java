5965d835363413ac387d16a2dc11db71d6aea3db
인택 조 <dlsxor21c@naver.com>
2018-03-17 16:27:26 +0900

    test2

--- Week2/src/simpleCal.java
+++ Week2/src/simpleCal.java
@@ -1,18 +1,46 @@
 
 public class simpleCal {
 	static int resultNumber;
+	
+	/**
+	* num1과 num2를 합하고 그 결과를 반환한다.
+	* @param num1 더하기 연산을 위한 첫번째 피연산자
+	* @param num2 더하기 연산을 위한 두번째 피연산자
+	* @return num1 + num2 값을반환
+	*/
 	public static int addNumber(int num1, int num2) {
 		resultNumber= num1 + num2;
 	return resultNumber;
 	}
+	
+	/**
+	* num1과 num2를 빼고 그 결과를 반환한다.
+	* @param num1 빼기 연산을 위한 첫번째 피연산자
+	* @param num2 빼기 연산을 위한 두번째 피연산자
+	* @return num1 - num2 값을 반환
+	*/
 	public static int subNumber(int num1, int num2) {
 		resultNumber= num1 - num2;
 	return resultNumber;
 	}
+	
+	/**
+	* num1과 num2를 곱하고 그 결과를 반환한다.
+	* @param num1 곱하기 연산을 위한 첫번째 피연산자
+	* @param num2 곱하기 연산을 위한 두번째 피연산자
+	* @return num1 * num2 값을 반환
+	*/
 	public static int mulNumber(int num1, int num2) {
 		resultNumber= num1 * num2;
 	return resultNumber;
 	}
+	
+	/**
+	* num1과 num2를 나누고 그 결과를 반환한다.
+	* @param num1 나누기 연산을 위한 첫번째 피연산자
+	* @param num2 나누기 연산을 위한 두번째 피연산자
+	* @return num1 / num2 값을 반환
+	*/
 	public static int divNumber(int num1, int num2) {
 		resultNumber= num1 / num2;
 	return resultNumber;
