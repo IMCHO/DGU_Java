f2cff353e54bd49eb408a07c327e580d346e655e
인택 조 <dlsxor21c@naver.com>
2018-03-25 21:33:37 +0900

    Modify Practice2!

--- Week3/src/Practice2.java
+++ Week3/src/Practice2.java
@@ -1,13 +1,8 @@
-import java.math.*;
-
 public class Practice2 {
-	public static BigInteger sum(long n1,long n2) {  // long 매개변수를 2개 받은 후 더한 값 리턴
-		String s1=String.valueOf(n1);                // long변수를 String변수로 변환
-		String s2=String.valueOf(n2);
-		BigInteger temp1=new BigInteger(s1);         // BigInteger 변수를 이용하여 계산
-		BigInteger temp2=new BigInteger(s2);
-		BigInteger addResult=temp1.add(temp2);       // 합 계산
-		return addResult;
+	public static double sum(long n1,long n2) {  // long 매개변수를 2개 받은 후 더한 값 리턴
+		double temp1=n1;
+		double temp2=n2;
+		return temp1+temp2;
 	}
 	public static void main(String[] args) {   // main 함수
 		long num1=5555555555555555555L;        // long 변수
