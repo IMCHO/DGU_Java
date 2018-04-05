9f4ed746e5ed90ccba9f4e755b6713146d0c85f6
인택 조 <dlsxor21c@naver.com>
2018-04-05 16:03:27 +0900

    Edit: Change code for practice condition

--- Week5/src/Practice2.java
+++ Week5/src/Practice2.java
@@ -1,18 +1,15 @@
-import java.util.Scanner;
 
 public class Practice2 {
 	public static void main(String[] arg) {
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt(); // int 자료형 변수 입력받기
 		int result = 0;
-		for (int i = 1; i < num; i++) {
-			if (num % i == 0)
-				result += i; // 나머지가 0이라면 num의 약수이므로 result 변수에 더해줌
+		for (int j = 1; j <= 100; j++) { // 1부터 100 사이 검사
+			for (int i = 1; i < j; i++) {
+				if (j % i == 0)
+					result += i; // 나머지가 0이라면 num의 약수이므로 result 변수에 더해줌
+			}
+			if (j == result)
+				System.out.print(j + " "); // 완전수라면 출력
+			result = 0; // 비교값 초기화
 		}
-		if (num == result)
-			System.out.println(num + "은(는) 완전수"); // 완전수의 경우
-		else
-			System.out.println(num + "은(는) 완전수가 아님"); // 완전수가 아닌 경우
-		sc.close();
 	}
 }
