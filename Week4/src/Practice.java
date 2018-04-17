e70772cdfc3e577da1847cc4c5065c84614a8055
인택 조 <dlsxor21c@naver.com>
2018-04-18 01:22:31 +0900

    Edit: Add exception handling, but not complie when file not exist

--- Week4/src/Practice.java
+++ Week4/src/Practice.java
@@ -64,10 +64,10 @@ public class Practice {
 	}
 
 	public static void loadFile() {
-		Scanner sc2 = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		boolean isCorrectPath = true; // 반복조건 플래그
 		while (isCorrectPath) {
-			INPUT_PATH = sc2.nextLine();
+			INPUT_PATH = sc.nextLine();
 			try {
 				inputStream = new FileInputStream(INPUT_PATH); // 입력된 경로값으로 new 할당
 				bufferedInputStream = new BufferedInputStream(inputStream, BUFFER_SIZE);
@@ -104,7 +104,7 @@ public class Practice {
 		Scanner in = new Scanner(System.in); // 문자열 입력
 		input = in.next(); // 입력한 문자열 input에 할당
 		int word_count = 0; // 단어 개수를 세기 위한 변수
-		result = INPUT_PATH + " 파일에서 " + input + "의 검색 결과는 다음과 같습니다.\n"; // 결과를 저장해 줄 변수
+		result = "\r\n"+INPUT_PATH + " 파일에서 " + input + "의 검색 결과는 다음과 같습니다.\r\n"; // 결과를 저장해 줄 변수
 
 		// System.out.println(strTemp[64897]);
 		for (int i = 0; i < count; i++) {
@@ -115,7 +115,7 @@ public class Practice {
 			for (int j = 0; j < splitStrArr.length; j++) {
 
 				if (input.matches(splitStrArr[j])) {
-					result += (i + 1) + "번쨰 줄 " + (j + 1) + "번째\n"; // 검색단어와 저장되어있는 단어와 같은지 비교
+					result += (i + 1) + "번쨰 줄 " + (j + 1) + "번째\r\n"; // 검색단어와 저장되어있는 단어와 같은지 비교
 					word_count++;
 				}
 			}
@@ -123,16 +123,20 @@ public class Practice {
 			// break;
 		}
 
-		result += "이상 검색결과로 총 " + word_count + "개의 검색이 완료되었습니다.\n";
+		result += "이상 검색결과로 총 " + word_count + "개의 검색이 완료되었습니다.\r\n";
 		System.out.println(result); // 결과 출력
 	}
 
 	public static void writeFile() {
 		Scanner in = new Scanner(System.in); // 문자열 입력
-		OUTPUT_PATH=in.nextLine();
+		OUTPUT_PATH = in.nextLine();
 		File file = new File(OUTPUT_PATH);
-		//FileOutputStream fos=new FileOutputStream(file);
-		if(file.exists()) {
+		FileWriter filewriter;
+		// FileOutputStream fos = new FileOutputStream(OUTPUT_PATH);
+
+		// BufferedWriter bufferedwriter=new BufferedWriter(filewriter);
+		if (file.exists()) { // 파일이 존재하면 붙여넣기 유무 물어보기
+
 			System.out.println("붙여 넣기 하시겠습니까?(y/n)");
 			String findWord;
 			boolean isExitProgram = false; // 프로그램 종료 플래그
@@ -141,13 +145,30 @@ public class Practice {
 				switch (findWord) {
 				case "Y":
 				case "y": {
-					
+
+					try {
+						filewriter = new FileWriter(file, true); // 붙여넣기
+						filewriter.write(result);
+						filewriter.close();
+					} catch (IOException e) {
+						// TODO 자동 생성된 catch 블록
+						e.printStackTrace();
+					}
+
 					isExitProgram = true;
 					break;
 				}
 				case "N":
 				case "n": {
-					file.w
+					try {
+						filewriter = new FileWriter(file, false); // 덮어쓰기
+						filewriter.write(result);
+						filewriter.close();
+					} catch (IOException e) {
+						// TODO 자동 생성된 catch 블록
+						e.printStackTrace();
+					}
+
 					isExitProgram = true;
 					break;
 				}
@@ -155,10 +176,26 @@ public class Practice {
 					System.out.println("알 수 없는 명령입니다. 한번 더 입력해주세요!");
 					break;
 				}
+			}
+		} else {
+			try {
+				filewriter = new FileWriter(file, false); // 새로 쓰기
+				filewriter.write(result);
+				filewriter.close();
+			} catch (IOException e) {
+				// TODO 자동 생성된 catch 블록
+				e.printStackTrace();
+			}
+
 		}
+
 	}
-		else {
-			
-		}
-}
+
+	public static boolean isExistFile(String str) { // 파일이 존재하는지 확인해주는 메소드
+		File file = new File(str);
+		if (file.exists())
+			return true;
+		else
+			return false;
+	}
 }
